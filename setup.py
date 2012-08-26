2cdadf0fa3685062c197b5e22291a6c0147fdad1
Paul Engstler <hi@paulengstler.com>
2012-08-26 17:14:37 +0200

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name="glacier",
-      version="0.1",
+      version="0.11",
       description="library to access Amazon Glacier",
       license="MIT",
       author="Paul Engstler",
